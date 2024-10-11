566d467f3dfe133b0a09802fd9320bebb1fa246f
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-10-11 18:37:18 +0200

    added style

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 require_once __DIR__ . '/db/data.php';
 
-echo "<script>const data = JSON.parse('$json_data'); </script>";
+echo "<script>const data = JSON.parse($json_data); </script>";
 
 ?>
  
@@ -12,6 +12,8 @@ echo "<script>const data = JSON.parse('$json_data'); </script>";
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Movie OOP</title>
   <!-- Link -->
+   <!-- Font awesome -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Custom style -->
   <link rel="stylesheet" href="./style/style.css">
 </head>
@@ -19,9 +21,44 @@ echo "<script>const data = JSON.parse('$json_data'); </script>";
   
   
   <div id="app">
+    <header>
+      <!-- header logo -->
+      <h1 class="Logo">Quattro zampe</h1>
+      <!-- central navbar -->
+      <nav>
+        <ul>
+          <li>Perhé scegliere
+            <div>
+              Quattro zampe
+            </div>
+          </li>
+          <li>Cani</li>
+          <li>Gatti</li>
+          <li class="search-box">
+            <form action="">
+              <label for="product-input">
+                <i class="fa-solid fa-magnifying-glass"></i>
+              </label>
+              <input type="text" placeholder="Cerca..." name="product-input" id="product-input">
+            </form>
+          </li>
+        </ul>
+      </nav>
+      <!-- Login box and modal window -->
+      <div class="login-box">
+        <div class="text-wrapper">
+          Accedi o<br>registrati
+        </div>
+        <div class="buttons-wrapper">
+          <button>
+            <i class="fa-regular fa-user"></i>
+          </button>
+        </div>
+      </div>
+    </header>
+
     <main>
       <div class="container-md">
-        <h1>E-commerce degli animali</h1>
 
         <!-- Contenuti mostrati sotto forma di elenco di singoli blocchi o card -->
           <ul class="row products-list">
